cf6940be64849177b9dedfdc6fdb509cca2e0e85
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-05-24 07:12:29 +0600

    Skills done

--- edu/views.py
+++ edu/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def skills(request):
+    return render(request, 'edu/skills.html')
--- resume_project/urls.py
+++ resume_project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('services/', include('service.urls')),
+    path('skills/', include('edu.urls')),
 ]
